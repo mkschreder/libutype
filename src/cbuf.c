5583e82ff62194be56526d80d6ecad333219d886
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-14 22:50:39 +0200

    add cbuf.c to makefile

--- src/cbuf.c
+++ src/cbuf.c
@@ -1,5 +1,4 @@
 #include "cbuf.h"
-#include <arch/soc.h>
 
 #define _cbuf_hmt(name) ((int16_t)(name)->head - (int16_t)(name)->tail)
 
@@ -15,59 +14,30 @@ void cbuf_init(struct cbuf *self, char *buffer, uint16_t size){
 }
 
 void cbuf_clear(struct cbuf *self){
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		self->head = self->tail = 0; 
-	}
+	self->head = self->tail = 0; 
 }
 
-uint16_t cbuf_get_waiting_isr(struct cbuf *self){
+uint16_t cbuf_get_waiting(struct cbuf *self){
 	int16_t hmt = _cbuf_hmt(self); 
 	return (hmt < 0)?(hmt + ((self)->size)):hmt; 
 }
 
-uint16_t cbuf_get_waiting(struct cbuf *self){
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		return cbuf_get_waiting_isr(self); 
-	}
-	return 0; 
-}
-
 uint16_t cbuf_get_free(struct cbuf *self){
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		return self->size - cbuf_get_waiting(self) - 1; 
-	}
-	return 0; 
+	return self->size - cbuf_get_waiting(self) - 1; 
 }
 
 uint8_t cbuf_is_empty(struct cbuf *self){
-	//ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		return self->head == self->tail; 
-	//}
-	//return 0; 
-}
-
-uint8_t cbuf_is_full_isr(struct cbuf *self){
-	return ((self)->head == (((self)->tail - 1) & ((self)->size - 1))); 
+	return self->head == self->tail; 
 }
 
 uint8_t cbuf_is_full(struct cbuf *self){
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		return cbuf_is_full_isr(self); 
-	}
-	return 0; 
-}
-
-uint16_t cbuf_get_isr(struct cbuf *self){
-	if(self->head == self->tail) return CBUF_NO_DATA; 
-	self->tail = (self->tail + 1) & (self->size - 1); 
-	return self->buffer[self->tail]; 
+	return ((self)->head == (((self)->tail - 1) & ((self)->size - 1))); 
 }
 
 uint16_t cbuf_get(struct cbuf *self){
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		return cbuf_get_isr(self); 
-	}
-	return CBUF_NO_DATA; 
+	if(self->head == self->tail) return CBUF_NO_DATA; 
+	self->tail = (self->tail + 1) & (self->size - 1); 
+	return self->buffer[self->tail]; 
 }
 
 uint8_t cbuf_put_isr(struct cbuf *self, uint8_t data){
@@ -78,10 +48,10 @@ uint8_t cbuf_put_isr(struct cbuf *self, uint8_t data){
 }
 
 uint8_t cbuf_put(struct cbuf *self, uint8_t data){
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		return cbuf_put_isr(self, data); 
-	}
-	return 0; 
+	if(((self)->head == (((self)->tail - 1) & ((self)->size - 1)))) return 0; 
+	self->head = (self->head + 1) & (self->size - 1); 
+	self->buffer[self->head] = data; 
+	return 1; 
 }
 
 uint16_t cbuf_getn(struct cbuf *self, char *data, uint16_t size){
@@ -105,9 +75,7 @@ uint16_t cbuf_putn(struct cbuf *self, const char *data, uint16_t size){
 
 uint16_t cbuf_peek(struct cbuf *self, uint16_t pos){
 	if(cbuf_get_waiting(self) >= pos) return CBUF_NO_DATA; 
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-		int16_t i = (self->tail + pos + 1) & (self->size - 1); 
-		return self->buffer[i]; 
-	}
-	return CBUF_NO_DATA; 
+
+	int16_t i = (self->tail + pos + 1) & (self->size - 1); 
+	return self->buffer[i]; 
 }
